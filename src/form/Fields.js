151064ef5eb39d5e880a0d809c77a12ff4475813
aaronamm <theeranitp@gmail.com>
2023-08-31 22:10:24 +0700

    More complex fields

--- src/form/Fields.js
+++ src/form/Fields.js
@@ -1,7 +1,100 @@
+import { useState, useSyncExternalStore } from "react";
 import { useFormContext } from "./FormContext";
+import { sprintf } from 'sprintf-js';
+
+export function getField(field) {
+  switch (field.type) {
+    case "splittedText":
+      return <SplittedTextField {...field} />
+    case "currency":
+      return <CurrencyField {...field} />
+    case "text":
+      return <TextField {...field} />
+    case "fieldGroup":
+      return <FieldGroup {...field} />
+    case "checkbox":
+      return <Checkbox {...field} />
+    case "specifiedValueCheckbox":
+      return <SpecifiedValueCheckbox {...field} />
+    case "remark":
+      return <Remark {...field} />
+    default:
+      return <></>
+  }
+};
+
+export function SplittedTextField({ name, label, placeholder, template, ...rest }) {
+  const { state, dispatch } = useFormContext();
+  const field = state[name];
+
+  const handleChanged = (e) => {
+    let payload;
+    if (e.target.value) {
+      const renderedTemplate = e.target.value.split(/,|\s+/).filter(v => v.trim()).map(v => sprintf(template, v)).join(' ');
+      payload = { value: e.target.value, template: renderedTemplate };
+    } else {
+      payload = { value: null, template: null };
+    }
+    dispatch({ type: name, payload });
+  };
+
+  return (
+    <>
+      <div>
+        {label && <label htmlFor={name}>{label}</label>}
+      </div>
+      <div>
+        <input type='text'
+          className="form-control"
+          placeholder={placeholder || ""}
+          {...rest}
+          value={field.value || ''}
+          onChange={handleChanged}
+        />
+      </div>
+      <div>Todo error message</div>
+    </>
+  )
+}
+
+export function CurrencyField({ name, label, placeholder, template, ...rest }) {
+  const { state, dispatch } = useFormContext();
+  const field = state[name];
+  const numberFormat = new Intl.NumberFormat('en-US')
+
+  const handleChanged = (e) => {
+    let payload;
+    const textValue = e.target.value;
+    if (textValue) {
+      const renderedTemplate = sprintf(template, numberFormat.format(Number.parseFloat(textValue.replace(/,/g, ''))));
+      payload = { value: textValue, template: renderedTemplate };
+    } else {
+      payload = { value: null, template: null };
+    }
+    dispatch({ type: name, payload });
+  };
+
+  return (
+    <>
+      <div>
+        {label && <label htmlFor={name}>{label}</label>}
+      </div>
+      <div>
+        <input type='text'
+          className="form-control"
+          name={name}
+          placeholder={placeholder || ""}
+          {...rest}
+          value={field.value || ''}
+          onChange={handleChanged}
+        />
+      </div>
+      <div>Todo error message</div>
+    </>
+  )
+}
 
 export function TextField({ name, label, placeholder, template, ...rest }) {
-
   const { state, dispatch } = useFormContext();
   const field = state[name];
   return (
@@ -23,3 +116,137 @@ export function TextField({ name, label, placeholder, template, ...rest }) {
     </>
   )
 }
+
+export function Remark({ name, label, placeholder, ...rest }) {
+  const { state, dispatch } = useFormContext();
+  //const [value, setValue] = useState('');
+  const field = state[name];
+
+  const handleChanged = (e) => {
+    //const remarkValue = e.target.value;
+    //setValue(e.target.value);
+    dispatch({ type: name, payload: { remark: e.target.value } });
+  };
+
+  return (
+    <>
+      <div>
+        {label && <label >{label}</label>}
+      </div>
+      <div>
+
+        <input type='text'
+          className='form-control'
+          placeholder={placeholder || ''}
+          {...rest}
+          value={field.remark || ''}
+          onChange={handleChanged}
+        />
+      </div>
+
+    </>
+  )
+}
+
+export function FieldGroup({ name, label, items }) {
+
+  const [selectedOption, setSelectOption] = useState('');
+  const [template, setTemplate] = useState('');
+
+  return (
+    <div>
+      <div>
+        group
+        {label && <label>{label}</label>}
+      </div>
+      <ul>
+        {items.map((item, index) => {
+          item.name = name;
+          return (<li key={`${name}_${index}`}> {getField(item)} </li>);
+        })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export function Checkbox({ name, label, placeholder, template, value, ...rest }) {
+
+  const { state, dispatch } = useFormContext();
+  const field = state[name];
+
+  const handleChanged = (e) => {
+    var newValue = field.value == e.target.value ? null : e.target.value
+
+    console.log({ newValue, value });
+    dispatch({ type: name, payload: { value: newValue, template: template } });
+  };
+
+  return (
+    <>
+      <div>
+        {label && <label htmlFor={name}>{label}</label>}
+      </div>
+      <div>
+        <input
+          value={value}
+          checked={value === field.value}
+          onChange={handleChanged}
+          type="checkbox"
+        />
+        option
+      </div>
+    </>
+  )
+};
+
+export function SpecifiedValueCheckbox({ name, label, placeholder, template, value, ...rest }) {
+
+  const { state, dispatch } = useFormContext();
+  const field = state[name];
+
+  const [textValue, setTextValue] = useState(value);
+
+  const handleCheckInputChanged = (e) => {
+    var newValue = field.value == e.target.value ? null : e.target.value
+    if (newValue == null) {
+      dispatch({ type: name, payload: { value: newValue, template: null } });
+    } else {
+      const renderedTemplate = sprintf(template, textValue)
+      dispatch({ type: name, payload: { value: newValue, template: renderedTemplate } });
+    }
+
+  };
+
+  const handleTextInputChanged = (e) => {
+    if (field.value === value) {
+      const renderedTemplate = sprintf(template, e.target.value)
+      dispatch({ type: name, payload: { value: field.value, template: renderedTemplate } });
+
+      setTextValue(e.target.value)
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {label && <label htmlFor={name}>{label}</label>}
+      </div>
+      <div>
+        <input
+          value={value}
+          checked={value === field.value}
+          onChange={handleCheckInputChanged}
+          type="checkbox"
+        />
+
+        <input type='text'
+          className="form-control"
+          placeholder={placeholder || ""}
+          onChange={handleTextInputChanged}
+        />
+
+      </div>
+    </>
+  )
+};
--- src/form/index.js
+++ src/form/index.js
@@ -1,11 +1,10 @@
 // https://www.pluralsight.com/guides/generating-dynamic-forms-from-json-in-react
 
-import React, { useCallback, useReducer } from "react";
+import React, { useReducer } from "react";
 import { FormContext } from "./FormContext";
 import { sprintf } from 'sprintf-js';
-
 import formSchema from './FormSchema.json'
-import { TextField } from "./Fields";
+import { getField } from "./Fields";
 
 const initialState = formSchema.reduce((state, field) => {
   state = {
@@ -18,53 +17,51 @@ const initialState = formSchema.reduce((state, field) => {
   return state;
 }, {});
 
+function GetNewField(changedProperty, action) {
+
+  if (action.payload.remark) {
+    return { ...changedProperty, remark: action.payload.remark };
+  }
+
+  if (action.payload.value == null) {
+    return { value: null, renderedTemplate: '' };
+  }
+
+  const renderedTemplate = sprintf(action.payload.template, `${action.payload.value}`)
+  return { ...changedProperty, value: action.payload.value, renderedTemplate };
+}
 
 export const FormReducer = (state, action) => {
   const changedProperty = state[action.type];
   if (changedProperty) {
-    const renderedTemplate = sprintf(action.payload.template, `${action.payload.value}`)
-    return { ...state, [action.type]: { ...changedProperty, value: action.payload.value, renderedTemplate } }
+    return { ...state, [action.type]: GetNewField(changedProperty, action) }
   } else {
     return state;
   }
 };
 
-const getFormElement = (field) => {
-  switch (field.type) {
-    case "text":
-      return <TextField {...field} />
-    default:
-      return <></>
-  }
-}
-
-
 const Form = ({ }) => {
   const [state, dispatch] = useReducer(FormReducer, initialState);
 
   return (
-    <div>
-
+    <>
       <FormContext.Provider value={{ state, dispatch }}>
         <form>
           {formSchema.map((field) => {
-
             return (
               <div key={field.name}>
-                {getFormElement(field)}
+                {getField(field)}
               </div>
             );
           })
           }
         </form>
-
-
       </FormContext.Provider>
 
       <div>
         {JSON.stringify(state, null, 2)}
       </div>
-    </div>
+    </>
   );
 };
 
